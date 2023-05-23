abcf1c909d4532d80acc3939cbdda6b73069a8d4
Disolm <disolm@gmail.com>
2023-05-23 15:10:50 +0700

    Glebskii-task_2

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,22 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = createApp({
+  name: 'app',
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    counterAdd() {
+      this.counter += 1
+    }
+  },
+  template:
+    `<button
+      type="button"
+      @click="counterAdd"
+    >
+      {{ counter }}
+    </button>`,
+}).mount('#app')
--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,52 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = createApp({
+  name: 'app',
+  data(){
+    return{
+      action: undefined,
+      number: {
+        left: 0,
+        right: 0
+      },
+      inputRadios: [
+        {
+          value: 'sum',
+          symbol: '➕'
+        },
+        {
+          value: 'subtract',
+          symbol: '➖'
+        },
+        {
+          value: 'multiply',
+          symbol: '✖'
+        },
+        {
+          value: 'divide',
+          symbol: '➗'
+        },
+      ]
+    }
+  },
+  computed:{
+    result() {
+      return this.calculation()
+    },
+  },
+  methods:{
+    calculation(){
+      if(!this.action) {
+        return 0
+      } else if(this.action === 'sum') {
+        return this.number.left + this.number.right
+      } else if(this.action === 'subtract') {
+        return this.number.left - this.number.right
+      } else if(this.action === 'multiply') {
+        return this.number.left * this.number.right
+      } else if(this.action === 'divide') {
+        return this.number.left / this.number.right
+      }
+    }
+  },
+}).mount('#app')
